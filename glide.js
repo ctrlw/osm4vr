c6c703406d6a9c09f4608bfe2d14eb5b19c7d955
ctrlw <markus.graeser+gh@gmail.com>
2024-02-08 18:26:18 +0100

    refactor: use vector arithmetics, needs further adaptation to feel good

--- glide.js
+++ glide.js
@@ -17,86 +17,78 @@ AFRAME.registerComponent('log-position', {
     this.rigRot = document.getElementById('rig').object3D.rotation;
     this.head = document.getElementById('head').object3D.position;
     
-    // track controller movement of left and right
-    this.lx = this.leftHand.x;
-    this.ly = this.leftHand.y; // y is height
-    this.lz = this.leftHand.z;
-    this.rx = this.rightHand.x;
-    this.ry = this.rightHand.y; // y is height
-    this.rz = this.rightHand.z;
+    // track movement of left and right controllers and rig
+    this.oldLeft = this.leftHand.clone();
+    this.oldRight = this.rightHand.clone();
+    this.dir = new THREE.Vector3(0, 0, 0);
 
-    this.xdir = 0;
-    this.zdir = 0;
+    // this.VAXIS = new THREE.Vector3(0, 1, 0);
   },
   
   tick: function () {
-    var rotation = this.el.getAttribute('rotation');
+    var rot = this.el.getAttribute('rotation');
     var pos = this.el.getAttribute('position');
-    var s = "rot: " + toString(rotation) + "\npos: " + toString(pos);
+    var s = "rot: " + toString(rot) + "\npos: " + toString(pos);
     console.log(s);
     var target = this.data.target; // log to this element
     const hud = document.getElementById(target);
     hud.setAttribute('text', 'value: ' + s);
 
+    let dir = new THREE.Vector3(0, 0, 0);
+
     let isLeftController = (target == 'lhud');
     let isRightController = (target == 'rhud');
     if (isLeftController || isRightController) {
-      let xd = isLeftController ? this.lx - pos.x : this.rx - pos.x;
-      let yd = isLeftController ? this.ly - pos.y : this.ry - pos.y;
-      let zd = isLeftController ? this.lz - pos.z : this.rz - pos.z;
-      // this.rig.y = pos.y; // move height with right controller height
-      
+      let inverseControllerDir = new THREE.Vector3(0, 0, 0);
+      if (isLeftController) {
+        inverseControllerDir = this.oldLeft.sub(this.leftHand);
+      } else {
+        inverseControllerDir = this.oldRight.sub(this.rightHand);
+      }
+
       // glide down while above start height
       let isFlying = this.rig.y > 0;
       if (isFlying) {
-        this.rig.y -= 0.01;
+        dir.y -= 0.01;
       }
       
       // go up when moving down the controller
-      let isControllerMovingDown = yd > 0;
-      let xdir = 0;
-      let zdir = 0;
+      let isControllerMovingDown = inverseControllerDir.y > 0;
       if (isControllerMovingDown) {
         // move up when controller is moving down, move faster with wider arm span
-        var xdelta = Math.abs(this.leftHand.x - this.rightHand.x) + 0.2;
-        var zdelta = Math.abs(this.leftHand.z - this.rightHand.z) + 0.2;
-        var armWidth = xdelta * xdelta + zdelta * zdelta;
-        this.rig.y += yd * armWidth;
+        let armWidth = this.leftHand.distanceTo(this.rightHand);
+        dir.y += inverseControllerDir.y * armWidth;
 
         let factor = 3;
-        xdir = xd * factor;
-        zdir = zd * factor;
+        dir.x = inverseControllerDir.x * factor;
+        dir.z = inverseControllerDir.z * factor;
         
-        this.xdir += xdir * 0.1;
-        this.zdir += zdir * 0.1;
+        // add 10% of the controller direction to the gliding direction
+        this.dir.x += dir.x * 0.1;
+        this.dir.z += dir.z * 0.1;
       }
       
       // add floating
       if (this.rig.y > 0.2) { // only move horizontally if we're above ground
         // slow down horizontal movement with some resistance
         let slow_down_factor = 0.98;
-        if (this.xdir * this.xdir + this.zdir * this.zdir > 0.001) {
-          this.xdir *= slow_down_factor;
-          this.zdir *= slow_down_factor;
+        if (this.dir.lengthSq() > 0.001) {
+          this.dir.x *= slow_down_factor;
+          this.dir.z *= slow_down_factor;
         }
-
-        this.rig.x += this.xdir + xdir * 2;
-        this.rig.z += this.zdir + zdir * 2;
       } else {
         // stop gliding when we're on the ground
-        this.xdir = 0;
-        this.zdir = 0;
+        this.dir = new THREE.Vector3(0, 0, 0);
       }
+
+      this.rig.add(this.dir);
+      this.rig.add(dir);
       
       // remember controller position
       if (isLeftController) {
-        this.lx = pos.x;
-        this.ly = pos.y;
-        this.lz = pos.z;
+        this.oldLeft.copy(this.leftHand);
       } else {
-        this.rx = pos.x;
-        this.ry = pos.y;
-        this.rz = pos.z;
+        this.oldRight.copy(this.rightHand);
       }
     }
   }
