1bcfefd6293432f63a33cb2812543974230a4419
ctrlw <markus.graeser+gh@gmail.com>
2024-06-18 11:25:54 +0200

    Show OSM tiles on ground plane

--- geojson.js
+++ geojson.js
@@ -43,7 +43,8 @@ function features2center(features) {
 
 // Convert geocoordinates into meter-based positions around the given base
 // coordinates order in geojson is longitude, latitude!
-function coords2plane(coords, baseLat, baseLon) {
+// coords is a path of [lon, lat] positions, e.g. [[13.41224,52.51712],[13.41150,52.51702],...] 
+function geojsonCoords2plane(coords, baseLat, baseLon) {
   let circumference_m = EQUATOR_M * Math.cos(baseLat * Math.PI / 180);
   return coords.map(([lon, lat]) => [
     (lon - baseLon) / 360 * circumference_m,
@@ -117,10 +118,10 @@ function feature2color(feature) {
 // baseLat and baseLon are used as reference position to convert geocoordinates to meters on plane
 function feature2building(feature, baseLat, baseLon) {
   let paths = feature.geometry.coordinates;
-  let xyOutline = coords2plane(paths[0], baseLat, baseLon);
+  let xyOutline = geojsonCoords2plane(paths[0], baseLat, baseLon);
   let xyHoles = []; // Add holes to the building if more than one path given
   for (let i = 1; i < paths.length; i++) {
-    xyHoles.push(coords2plane(paths[i], baseLat, baseLon));
+    xyHoles.push(geojsonCoords2plane(paths[i], baseLat, baseLon));
   }
   let height_m = feature2height(feature);
   let building = createBuilding(xyOutline, xyHoles, height_m);
@@ -133,6 +134,8 @@ function feature2building(feature, baseLat, baseLon) {
 
 AFRAME.registerComponent('geojson', {
   schema: {
+    lat: {type: 'number'},
+    lon: {type: 'number'},
     src: {type: 'asset'}
   },
 
@@ -152,13 +155,14 @@ AFRAME.registerComponent('geojson', {
 
   onSrcLoaded: function (text) {
     let json = JSON.parse(text);
-    let center = features2center(json.features);
-    let baseLat = center[0];
-    let baseLon = center[1];
+    if (this.data.lat == 0 && this.data.lon == 0) {
+      let center = features2center(json.features);
+      this.data.lat = center[0];
+      this.data.lon = center[1];
+    }
     for (let feature of json.features) {
       if ('building' in feature.properties) {
-        let coords = feature.geometry.coordinates[0];    
-        let building = feature2building(feature, baseLat, baseLon);
+        let building = feature2building(feature, this.data.lat, this.data.lon);
         // console.log(building);
         document.querySelector('a-scene').appendChild(building);
       }
