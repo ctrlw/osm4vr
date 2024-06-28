eecb76505d87d13f687e791938da533632072a72
ctrlw <markus.graeser+gh@gmail.com>
2024-06-29 00:00:05 +0200

    refactor: move log2hud code into component

--- log2hud.js
+++ log2hud.js
@@ -1,9 +1,4 @@
-function toString(pos) {
-  var x = pos.x.toFixed(1);
-  var y = pos.y.toFixed(1);
-  var z = pos.z.toFixed(1);
-  return [x, y, z].join(" ");
-}
+// A-Frame component that logs the position and rotation of an entity to a text element
 
 AFRAME.registerComponent('log2hud', {
   schema: {
@@ -17,8 +12,15 @@ AFRAME.registerComponent('log2hud', {
   tick: function () {
     var rotation = this.el.getAttribute('rotation');
     var pos = this.el.getAttribute('position');
-    var s = "pos: " + toString(pos) + "\nrot: " + toString(rotation);
+    var s = "pos: " + this.toString(pos) + "\nrot: " + this.toString(rotation);
     // console.log(s);
     this.hud.setAttribute('text', 'value: ' + s);
-  }
+  },
+
+  toString: function(pos) {
+    var x = pos.x.toFixed(1);
+    var y = pos.y.toFixed(1);
+    var z = pos.z.toFixed(1);
+    return [x, y, z].join(" ");
+  }  
 })
